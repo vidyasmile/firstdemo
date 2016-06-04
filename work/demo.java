276a244a2ef59a93079ca426f323bd69ef53399e
vidya <vidyasmile90@gmail.com>
2016-06-04 12:32:46 +0530

    added java

---
+++ work/demo.java
@@ -0,0 +1,13 @@
+
+public class demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("hai how are you");
+System.out.print("hai hw hw hw are you");
+	}
+
+}
